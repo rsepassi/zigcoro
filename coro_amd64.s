350a09553502fa36d16bfdae2c389bf3382dcd5c
Ryan Sepassi <rsepassi@gmail.com>
2023-08-28 18:32:09 -0700

    retab

--- coro_amd64.s
+++ coro_amd64.s
@@ -2,26 +2,26 @@
 .globl libcoro_stack_swap
 libcoro_stack_swap:
   # Store caller registers on the current stack
-	pushq %rbp
-	pushq %rbx
-	pushq %r12
-	pushq %r13
-	pushq %r14
-	pushq %r15
+  pushq %rbp
+  pushq %rbx
+  pushq %r12
+  pushq %r13
+  pushq %r14
+  pushq %r15
 
   # Modify stack pointer of current coroutine (rdi, first argument)
-	movq %rsp, (%rdi)
+  movq %rsp, (%rdi)
 
   # Load stack pointer from target coroutine (rsi, second argument)
-	movq (%rsi), %rsp
+  movq (%rsi), %rsp
 
   # Restore target registers
-	popq %r15
-	popq %r14
-	popq %r13
-	popq %r12
-	popq %rbx
-	popq %rbp
+  popq %r15
+  popq %r14
+  popq %r13
+  popq %r12
+  popq %rbx
+  popq %rbp
 
   # jump
-	ret
+  ret
