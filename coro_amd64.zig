44bfa470ae7ac7a02fdea4705c10b306e4c2d8ad
Ryan Sepassi <rsepassi@gmail.com>
2023-08-29 10:11:29 -0700

    try more cleanup

--- coro_amd64.zig
+++ coro_amd64.zig
@@ -7,17 +7,18 @@ const ArchInfo = struct {
 };
 const arch_info: ArchInfo = switch (@import("builtin").os.tag) {
     .windows => .{
-        .num_registers = 30,
+        .num_registers = 32,
+        .jump_idx = 30,
         .assembly = @embedFile("coro_amd64_windows.s"),
     },
     else => .{
-        .num_registers = 6,
+        .num_registers = 8,
+        .jump_idx = 6,
         .assembly = @embedFile("coro_amd64.s"),
     },
 };
 
 pub const stack_align = 16;
-const num_registers = arch_info.num_registers;
 
 extern fn libcoro_stack_swap(current: *Coro, target: *Coro) void;
 comptime {
@@ -36,24 +37,12 @@ pub const Coro = packed struct {
     pub fn init(func: Func, stack: []align(stack_align) u8) !Self {
         if (@sizeOf(usize) != 8) @compileError("usize expected to take 8 bytes");
         if (@sizeOf(*Func) != 8) @compileError("function pointer expected to take 8 bytes");
-
-        // Top of the stack is the end of stack
-        const sp = stack.ptr + stack.len;
-
-        // Define an aligned pointer to func at the top of the stack
-        const jump: *Func = @ptrFromInt(std.mem.alignBackward(
-            usize,
-            @intFromPtr(sp - @sizeOf(*Func)),
-            stack_align,
-        ));
-
-        const register_bytes = num_registers * 8;
-        const rp: [*]u8 = @ptrFromInt(@intFromPtr(jump) - register_bytes);
-        if (@intFromPtr(rp) < @intFromPtr(stack.ptr)) return Error.StackTooSmall;
-
-        jump.* = func;
-
-        // Set the stack pointer to the bottom of the register space
+        const register_bytes = arch_info.num_registers * 8;
+        if (register_bytes > stack.len) return Error.StackTooSmall;
+        const register_space = stack[stack.len - register_bytes ..];
+        const jump_ptr: *Func = @ptrCast(&register_space[arch_info.jump_idx]);
+        jump_ptr.* = func;
+        const rp = register_space.ptr;
         return .{ .stack_pointer = rp };
     }
 
