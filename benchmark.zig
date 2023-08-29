da8d010d7b4ccaf94ec44811c4b6565725e9423e
Ryan Sepassi <rsepassi@gmail.com>
2023-08-28 23:42:02 -0700

    Add guarded benchmark spawning many coroutines

--- benchmark.zig
+++ benchmark.zig
@@ -3,10 +3,14 @@ const libcoro = @import("libcoro");
 
 var num_bounces: usize = 0;
 
-fn test_fn() void {
+fn testFn() void {
     for (0..num_bounces) |_| {
         libcoro.xsuspend();
     }
+}
+
+fn suspendRepeat() void {
+    while (true) libcoro.xsuspend();
 }
 
 pub fn main() !void {
@@ -14,33 +18,67 @@ pub fn main() !void {
     defer _ = gpa.deinit();
     const allocator = gpa.allocator();
 
-    const stack_size: usize = 1024 * 2;
-    const stack = try allocator.alignedAlloc(u8, libcoro.stack_align, stack_size);
-    defer allocator.free(stack);
-
-    // warmup
-    num_bounces = 100_000;
+    // context switch benchmark
     {
-        var test_coro = try libcoro.xasync(test_fn, .{}, stack, .{});
-        for (0..num_bounces) |_| {
+        const stack_size: usize = 1024 * 2;
+        const stack = try allocator.alignedAlloc(u8, libcoro.stack_align, stack_size);
+        defer allocator.free(stack);
+
+        // warmup
+        num_bounces = 100_000;
+        {
+            var test_coro = try libcoro.xasync(testFn, .{}, stack, .{});
+            for (0..num_bounces) |_| {
+                libcoro.xresume(test_coro);
+            }
             libcoro.xresume(test_coro);
         }
-        libcoro.xresume(test_coro);
+
+        num_bounces = 20_000_000;
+        {
+            var test_coro = try libcoro.xasync(testFn, .{}, stack, .{});
+
+            const start = std.time.nanoTimestamp();
+            for (0..num_bounces) |_| {
+                libcoro.xresume(test_coro);
+            }
+            const end = std.time.nanoTimestamp();
+            const duration = end - start;
+            const ns_per_bounce = @divFloor(duration, num_bounces * 2);
+            std.debug.print("ns/ctxswitch: {d}\n", .{ns_per_bounce});
+
+            libcoro.xresume(test_coro);
+        }
     }
 
-    num_bounces = 20_000_000;
-    {
-        var test_coro = try libcoro.xasync(test_fn, .{}, stack, .{});
+    // number of coroutines benchmark
+    if (false) {
+        const num_coros = 700_000;
+        var coros = try allocator.alloc(*libcoro.Coro, num_coros);
+        defer allocator.free(coros);
 
-        const start = std.time.nanoTimestamp();
-        for (0..num_bounces) |_| {
-            libcoro.xresume(test_coro);
+        var buf = try allocator.alloc(u8, num_coros * 1024 * 8);
+        var fba = std.heap.FixedBufferAllocator.init(buf);
+        const alloc2 = fba.allocator();
+
+        for (0..num_coros) |i| {
+            const coro = try libcoro.xasyncAlloc(suspendRepeat, .{}, alloc2, null, .{});
+            coros[i] = coro.coro;
         }
-        const end = std.time.nanoTimestamp();
-        const duration = end - start;
-        const ns_per_bounce = @divFloor(duration, num_bounces * 2);
-        std.debug.print("ns/ctxswitch: {d}\n", .{ns_per_bounce});
 
-        libcoro.xresume(test_coro);
+        for (0..20) |i| {
+            const start = std.time.nanoTimestamp();
+            for (coros) |coro| {
+                libcoro.xresume(coro);
+            }
+            const end = std.time.nanoTimestamp();
+            const duration = end - start;
+            const ns_per_bounce = @divFloor(duration, num_coros * 2);
+            if (i > 5 and ns_per_bounce > 100) {
+                std.debug.print("ns/ctxswitch: {d}\n", .{ns_per_bounce});
+                @panic("swap memory being used");
+            }
+        }
+        std.debug.print("efficiently ran {d} coroutines\n", .{num_coros});
     }
 }
