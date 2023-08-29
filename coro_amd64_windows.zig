e0213f9aabfe5e2e43af0cc549ad3796d5fe911a
Ryan Sepassi <rsepassi@gmail.com>
2023-08-28 22:19:37 -0700

    First attempt at Windows support

---
+++ coro_amd64_windows.zig
@@ -0,0 +1,48 @@
+const std = @import("std");
+const Error = @import("errors.zig").Error;
+
+pub const stack_align = 16;
+const num_registers = 6;
+
+extern fn libcoro_stack_swap(current: *Coro, target: *Coro) void;
+comptime {
+    asm (@embedFile("coro_amd64.s"));
+}
+
+pub const Coro = packed struct {
+    stack_pointer: [*]u8,
+
+    const Self = @This();
+    const Func = *const fn (
+        from: *Coro,
+        self: *Coro,
+    ) callconv(.C) noreturn;
+
+    pub fn init(func: Func, stack: []align(stack_align) u8) !Self {
+        if (@sizeOf(usize) != 8) @compileError("amd64 usize expected to take 8 bytes");
+        if (@sizeOf(*Func) != 8) @compileError("amd64 function pointer expected to take 8 bytes");
+
+        // Top of the stack is the end of stack
+        const sp = stack.ptr + stack.len;
+
+        // Define an aligned pointer to func at the top of the stack
+        const jump: *Func = @ptrFromInt(std.mem.alignBackward(
+            usize,
+            @intFromPtr(sp - @sizeOf(*Func)),
+            stack_align,
+        ));
+
+        const register_bytes = num_registers * 8;
+        const rp: [*]u8 = @ptrFromInt(@intFromPtr(jump) - register_bytes);
+        if (@intFromPtr(rp) < @intFromPtr(stack.ptr)) return Error.StackTooSmall;
+
+        jump.* = func;
+
+        // Set the stack pointer to the bottom of the register space
+        return .{ .stack_pointer = rp };
+    }
+
+    pub inline fn resumeFrom(self: *Self, from: *Self) void {
+        libcoro_stack_swap(from, self);
+    }
+};
